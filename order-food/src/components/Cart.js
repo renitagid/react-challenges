c1e2f27673c060c8507fc412f13bd7bba0e85f07
renitagid <renita.y.ip@gmail.com>
2022-11-10 13:04:47 -0800

    good robot, bad robot

--- order-food/src/components/Cart.js
+++ order-food/src/components/Cart.js
@@ -5,9 +5,9 @@ const Cart = (props) => {
     <div style={{margin: 20, borderStyle: "solid", padding: 20, borderRadius: "3rem"}}>
       <h1>Cart</h1>
     <div>Your items:
-      <p>{props.cart.map((item, index)=>{
-        return <p>{item}</p>
-      })}</p></div>
+      <div>{props.cart.map((item, index)=>{
+        return <p>{index+1} {item}</p>
+      })}</div></div>
     <div>Your total cost: {props.cost}</div>
     </div>
   )
---
+++ robots/src/BadRobot.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const BadRobot = (props) => {
+let blah = "blah blah blah blah blah"
+return blah.substring(0, props.input.length)
+}
+
+export default BadRobot;
